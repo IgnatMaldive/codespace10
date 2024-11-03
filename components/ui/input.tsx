6ade590cd8415838b01579c466679f78a99680c5
Ignat Maldive <36450446+IgnatMaldive@users.noreply.github.com>
2024-11-03 15:34:39 +0000

    comment

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -2,6 +2,8 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
+// Disable the eslint rule for this line only
+/* eslint-disable-next-line @typescript-eslint/no-empty-interface */
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
 
